90637c33dc91560e58f1a45d8fe024761b5d1be4
realdegrees <>
2023-08-28 13:46:09 +0200

    initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,37 @@
+<script>
+</script>
+
+<svelte:head>
+	<title>Home</title>
+	<meta name="description" content="Company Organizer" />
+</svelte:head>
+
+<section>
+	<h1>
+		<span class="welcome">
+			Placeholder
+		</span>
+	</h1>
+</section>
+
+<style>
+	section {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		flex: 0.6;
+	}
+
+	h1 {
+		width: 100%;
+	}
+
+	.welcome {
+		display: block;
+		position: relative;
+		width: 100%;
+		height: 0;
+		padding: 0 0 calc(100% * 495 / 2048) 0;
+	}
+</style>
